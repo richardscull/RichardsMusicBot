418e2fab1bbf4f340600b16eb66f75178f57decc
ItsRichardScull <totallynotrichardscull@gmail.com>
2023-10-12 16:19:33 +0300

    chore: Clean code and refactor package.json

--- src/utils/numberWith.ts
+++ src/utils/numberWith.ts
@@ -1,3 +1,6 @@
+// NOTE: Both functions are the same, could be merged into one with a parameter for the separator
+// -richardscull
+
 export function numberWithSpaces(number: number | string) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 }
