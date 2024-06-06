5ee32eb95fd07a580cf5a115391a62e6a0852212
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-06-06 18:15:49 +0300

    ref: Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -20,54 +20,59 @@ export class Config {
     this.checkSpotifyVariables();
     this.checkGithubBranchUrl();
 
-    const { GITHUB_BRANCH_URL } = process.env;
-    const { DISCORD_TOKEN, DISCORD_ID } = process.env;
-    const {
-      SPOTIFY_ID,
-      SPOTIFY_SECRET,
-      SPOTIFY_REFRESH_TOKEN,
-      SPOTIFY_MARKET,
-    } = process.env;
+    this.updateConfig();
+  }
 
-    this.config = {
-      GITHUB_BRANCH_URL: process.env.GITHUB_BRANCH_URL!,
-      DISCORD_TOKEN: process.env.DISCORD_TOKEN!,
-      DISCORD_ID: process.env.DISCORD_ID!,
-      SPOTIFY_ID: process.env.SPOTIFY_ID!,
-      SPOTIFY_SECRET: process.env.SPOTIFY_SECRET!,
-      SPOTIFY_REFRESH_TOKEN: process.env.SPOTIFY_REFRESH_TOKEN!,
-      SPOTIFY_MARKET: process.env.SPOTIFY_MARKET!,
-    };
+  private updateConfig() {
+    const variables = [
+      'GITHUB_BRANCH_URL',
+      'DISCORD_TOKEN',
+      'DISCORD_ID',
+      'SPOTIFY_ID',
+      'SPOTIFY_SECRET',
+      'SPOTIFY_REFRESH_TOKEN',
+      'SPOTIFY_MARKET',
+    ];
+
+    this.config = variables.reduce(
+      (acc, variable) => {
+        acc[variable] = process.env[variable] as string;
+        return acc;
+      },
+      {} as Record<string, string>
+    );
   }
 
   private checkDiscordVariables() {
     const discordVariables = ['TOKEN', 'ID'];
-    if (
-      !discordVariables.every((variable) => process.env['DISCORD_' + variable])
-    ) {
-      throw new Error(
-        chalk.yellowBright('⚠️   Missing arguments in .env file!')
-      );
-    }
+    const isEveryVariablePresent = discordVariables.every(
+      (variable) => process.env['DISCORD_' + variable]
+    );
+
+    if (isEveryVariablePresent) return;
+
+    throw new Error(chalk.yellowBright('⚠️   Missing arguments in .env file!'));
   }
 
   private checkSpotifyVariables() {
     const spotifyVariables = ['ID', 'SECRET', 'REFRESH_TOKEN', 'MARKET'];
-    if (
-      !spotifyVariables.every((variable) => process.env['SPOTIFY_' + variable])
-    ) {
-      throw new Error(
-        chalk.yellowBright(
-          '⚠️   Missing spotify related arguments in .env file!'
-        )
-      );
-    }
+    const isEveryVariablePresent = spotifyVariables.every(
+      (variable) => process.env['SPOTIFY_' + variable]
+    );
+
+    if (isEveryVariablePresent) return;
+
+    throw new Error(
+      chalk.yellowBright('⚠️   Missing spotify related arguments in .env file!')
+    );
   }
 
   private checkGithubBranchUrl() {
     let { GITHUB_BRANCH_URL } = process.env;
+
     const defaultGithubBranch =
       'repos/richardscull/RichardsMusicBot/commits/master';
+
     GITHUB_BRANCH_URL = GITHUB_BRANCH_URL
       ? GITHUB_BRANCH_URL
       : defaultGithubBranch;
