75363371d56ec66f38976431240d44c5d85cde96
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-06-06 20:16:23 +0300

    feat: Add chapters, repeat export in subcommands index

--- src/commands/music/subcommands/index.ts
+++ src/commands/music/subcommands/index.ts
@@ -3,5 +3,7 @@ import * as play from './play.subcommand';
 import * as queue from './queue.subcommand';
 import * as skip from './skip.subcommand';
 import * as stop from './stop.subcommand';
+import * as chapters from './chapters.subcommand';
+import * as repeat from './repeat.subcommand';
 
-export { pause, play, queue, skip, stop };
+export { pause, play, queue, skip, stop, chapters, repeat };
